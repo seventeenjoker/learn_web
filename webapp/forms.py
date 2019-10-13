e70ba817fe6e68063d0ee77da0ee0fd35a83b46f
Victoria Kiseleva <kiseleva.victoria.u@gmail.com>
2019-10-13 13:25:05 +0300

    lesson_7

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
+# Форма логина для /login
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class=": "form-control"})
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class=": "form-control"})
